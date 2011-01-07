e0190a5695c94e4bb77a31e9783058d6dd6b5ff6
loic911 <lrollus@ulg.ac.be>
2011-01-07 13:43:17 +0000

    Test with Bamboo and a failed test

--- test/unit/be/cytomine/security/UserTests.groovy
+++ test/unit/be/cytomine/security/UserTests.groovy
@@ -14,4 +14,28 @@ class UserTests extends GrailsUnitTestCase {
     void testSomething() {
 
     }
+
+    void testValidEmail() {
+        mockDomain(User)
+        def today = new Date()
+        def user = new User(firstname:"John",lastname:"Doe",email:"johndoe@site.com", dateCreated:today,authority:"toto",password:"toto",username:"toto")
+        println user.errors.firstname
+        println user.errors.lastname
+        println user.errors.email
+        println user.errors.dateCreated
+        println user.errors.authority
+
+        if(user.validate()) {
+           println "ok"
+        }
+        else {
+            println "nok"
+            user.errors.allErrors.each {
+            println it
+            }
+        }
+
+        assertFalse 'validation should be OK', user.validate()
+    }
+
 }
