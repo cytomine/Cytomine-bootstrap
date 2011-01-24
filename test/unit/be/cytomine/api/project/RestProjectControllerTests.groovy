28a0d08b2eff467aacc97a87cf25eb85edbe7a5d
stevben <b.stevens@ulg.ac.be>
2011-01-24 14:20:17 +0000

    Add test classes corresponding to new classes of the previous commit

---
+++ test/unit/be/cytomine/api/project/RestProjectControllerTests.groovy
@@ -0,0 +1,17 @@
+package be.cytomine.api.project
+
+import grails.test.*
+
+class RestProjectControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ test/unit/be/cytomine/command/stack/TransactionTests.groovy
@@ -0,0 +1,17 @@
+package be.cytomine.command.stack
+
+import grails.test.*
+
+class TransactionTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
