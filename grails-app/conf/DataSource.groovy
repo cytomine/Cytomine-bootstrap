6780a4484742604ddfd2e65af87edd6eba596ee9
stevben <b.stevens@ulg.ac.be>
2011-01-25 09:12:24 +0000

    Recover datasource in order to use HSQL databse

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,33 +1,32 @@
 dataSource {
-  pooled = true
-  driverClassName = "com.mysql.jdbc.Driver"
-  username = "root"
-  password = ""
-  dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+   pooled = true
+   driverClassName = "org.hsqldb.jdbcDriver"
+   username = "sa"
+   password = ""
 }
 hibernate {
-  cache.use_second_level_cache = true
-  cache.use_query_cache = true
-  cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+   cache.use_second_level_cache = true
+   cache.use_query_cache = true
+   cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
-  development {
-    dataSource {
-      dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-      url = "jdbc:mysql://127.0.0.1:3306/cytominedev?useUnicode=yes&characterEncoding=UTF-8"
-    }
-  }
-  test {
-    dataSource {
-      dbCreate = "update"
-      url = "jdbc:mysql://127.0.0.1:3306/cytominetest?useUnicode=yes&characterEncoding=UTF-8"
-    }
-  }
-  production {
-    dataSource {
-      dbCreate = "update"
-      url = "jdbc:mysql://127.0.0.1:3306/cytomine?useUnicode=yes&characterEncoding=UTF-8"
-    }
-  }
+   development {
+       dataSource {
+           dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+           url = "jdbc:hsqldb:mem:devDB"
+       }
+   }
+   test {
+       dataSource {
+           dbCreate = "update"
+           url = "jdbc:hsqldb:mem:testDb"
+       }
+   }
+   production {
+       dataSource {
+           dbCreate = "update"
+           url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+       }
+   }
 }
