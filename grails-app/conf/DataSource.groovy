7858703163bd766abeb8e8b61dee1648dfe2752a
loic911 <lrollus@ulg.ac.be>
2011-01-25 12:56:23 +0000

    Datasource with postgres for server-marée

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,32 +1,33 @@
 dataSource {
-   pooled = true
-   driverClassName = "org.hsqldb.jdbcDriver"
-   username = "sa"
-   password = ""
+  pooled = true
+  driverClassName = "org.postgresql.Driver"
+  username = "postgres"
+  password = "postgres"
+  dialect = org.hibernatespatial.postgis.PostgisDialect
 }
 hibernate {
-   cache.use_second_level_cache = true
-   cache.use_query_cache = true
-   cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+  cache.use_second_level_cache = true
+  cache.use_query_cache = true
+  cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
-   development {
-       dataSource {
-           dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-           url = "jdbc:hsqldb:mem:devDB"
-       }
-   }
-   test {
-       dataSource {
-           dbCreate = "update"
-           url = "jdbc:hsqldb:mem:testDb"
-       }
-   }
-   production {
-       dataSource {
-           dbCreate = "update"
-           url = "jdbc:hsqldb:file:prodDb;shutdown=true"
-       }
-   }
+  development {
+    dataSource {
+      dbCreate = "create-drop"
+      url="jdbc:postgresql://localhost:5432/cytomine"
+    }
+  }
+  test {
+    dataSource {
+      dbCreate = "update"
+      url="jdbc:postgresql://localhost:5432/cytomine"
+    }
+  }
+  production {
+    dataSource {
+      dbCreate = "update"
+      url = "jdbc:mysql://127.0.0.1:3306/cytomine?useUnicode=yes&characterEncoding=UTF-8"
+    }
+  }
 }
