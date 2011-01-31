6b625a463e6c75872fa19da42fa56a25f1b5521f
loic911 <lrollus@ulg.ac.be>
2011-01-31 15:04:19 +0000

    AddAnnotationCommmand bis

---
+++ grails-app/domain/be/cytomine/command/AddAnnotationCommand.groovy
@@ -0,0 +1,40 @@
+package be.cytomine.command
+
+import be.cytomine.security.User
+import be.cytomine.project.Annotation
+import grails.converters.JSON
+
+class AddAnnotationCommand extends Command implements UndoRedoCommand {
+
+  def execute() {
+    def newAnnotation = Annotation.getAnnotationFromData(JSON.parse(postData))
+    if(newAnnotation.validate()) {
+      newAnnotation.save()
+      data = newAnnotation.encodeAsJSON()
+      return [data : [success : true , message:"ok", annotation : newAnnotation], status : 201]
+    } else {
+      return [data : [user : newAnnotation , errors : [newAnnotation.errors]], status : 403]
+
+    }
+  }
+
+  def undo() {
+    def annotationData = JSON.parse(data)
+    def annotation = Annotation.findById(annotationData.id)
+    annotation.delete()
+    return [data : null, status : 200]
+  }
+
+  def redo() {
+
+
+    println "data = " + data
+
+    def annotationData = JSON.parse(data)
+
+    def annotation = Annotation.getAnnotationFromData(JSON.parse(postData))
+    annotation.id = annotationData.id
+    annotation.save()
+    return [data : [annotation : annotation], status : 200]
+  }
+}
