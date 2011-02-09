57be2d9e0c8b75299e534152719daa7f41bcb0b9
loic911 <lrollus@ulg.ac.be>
2011-02-09 08:54:51 +0000

    Correct functionnal test

--- test/functional/AnnotationTests.groovy
+++ test/functional/AnnotationTests.groovy
@@ -3,6 +3,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage
 import com.gargoylesoftware.htmlunit.DefaultCredentialsProvider
 
 import be.cytomine.HttpClient
+import net.sf.json.JSONObject
 /**
  * Created by IntelliJ IDEA.
  * User: lrollus
@@ -13,17 +14,21 @@ import be.cytomine.HttpClient
 class AnnotationTests extends functionaltestplugin.FunctionalTestCase {
 
   void testGetAnnotationsWithCredential() {
-    HttpClient client = new HttpClient("http://localhost:8080/cytomine-web/api/scan.json","lrollus","password");
+    HttpClient client = new HttpClient("http://localhost:8080/cytomine-web/api/image.json","lrollus","password");
     client.connect("GET");
     //TODO: test if it is a JSON
+    String str = client.getResponseString()
+    def json = JSON.parse(str)
+    assert json instanceof JSONObject
     client.disconnect();
     assertEquals(200,client.getResponseCode())
   }
 
   void testGetAnnotationsWithoutCredential() {
-    HttpClient client = new HttpClient("http://localhost:8080/cytomine-web/api/scan.json","badlogin","badpassword");
+    HttpClient client = new HttpClient("http://localhost:8080/cytomine-web/api/image.json","badlogin","badpassword");
     client.connect("GET");
-    //TODO: test if it is NOT a JSON
+    def json = JSON.parse(client.getResponseString())
+    assert json instanceof JSONObject
     client.disconnect();
     assertEquals(401,client.getResponseCode())
   }
