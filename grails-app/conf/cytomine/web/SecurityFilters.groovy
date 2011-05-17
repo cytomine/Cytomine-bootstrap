b76c6ff204ee89dd415cdbc964524b9ad1861136
stevben <b.stevens@ulg.ac.be>
2011-05-17 08:55:15 +0000

    Fix CYTO-138

--- grails-app/conf/cytomine/web/SecurityFilters.groovy
+++ grails-app/conf/cytomine/web/SecurityFilters.groovy
@@ -4,20 +4,13 @@ class SecurityFilters {
   def springSecurityService
 
   def filters = {
-    all(controller:'*', action:'*') {
-      /*before = {
-         if(controllerName != "login" && actionName != "auth" && !springSecurityService.isLoggedIn()) {
-            redirect(controller:'login', action:'auth')
+    all(uri:'/api/**') {
+      before = {
+         if(!springSecurityService.isLoggedIn()) {
+            redirect(uri:'/')
             return false
          }
-      }*/
-      /*before = {
-        def error403 = request.requestURI.endsWith("403")
-        if(!error403 && (controllerName != "server" && actionName != "ping") && request.getParameter('action') != 'auth' && !springSecurityService.isLoggedIn()) {
-          redirect(url:grailsApplication.config.grails.serverURL+"/?action=auth#login")
-          return false
-        }
-      }*/
+      }
       after = {
 
       }
