ccad551482af71e3f15134625e2cbd3bfaac0ed9
stevben <b.stevens@ulg.ac.be>
2011-07-10 08:57:42 +0000

    Remove message parameters from AddAnnotationCommand

--- grails-app/domain/be/cytomine/ontology/Annotation.groovy
+++ grails-app/domain/be/cytomine/ontology/Annotation.groovy
@@ -13,190 +13,190 @@ import be.cytomine.project.Project
 
 class Annotation extends SequenceDomain implements Serializable {
 
-  String name
-  Geometry location
-  ImageInstance image
-  Double zoomLevel
-  String channels
-  User user
+    String name
+    Geometry location
+    ImageInstance image
+    Double zoomLevel
+    String channels
+    User user
 
-  static belongsTo = [ImageInstance]
-  static hasMany = [ annotationTerm: AnnotationTerm ]
+    static belongsTo = [ImageInstance]
+    static hasMany = [ annotationTerm: AnnotationTerm ]
 
-  static transients = ["cropURL", "boundaries"]
+    static transients = ["cropURL", "boundaries"]
 
-  static constraints = {
-    name(blank:true)
-    location(nullable:false)
-    zoomLevel(nullable:true)
-    channels(nullable:true)
-    user(nullable:false)
-  }
-
-  static mapping = {
-    id generator : "assigned"
-    columns {
-      location type: org.hibernatespatial.GeometryUserType
+    static constraints = {
+        name(blank:true)
+        location(nullable:false)
+        zoomLevel(nullable:true)
+        channels(nullable:true)
+        user(nullable:false)
     }
-  }
 
-  String toString() {return "Annotation " + id}
-
-  /**
-   * If name is empty, fill it by "Annotation $id"
-   */
-  public beforeInsert() {
-    super.beforeInsert()
-    name = name && !name.trim().equals("")? name : "Annotation " + id
-  }
-  /*public afterInsert() {
-    println "Annotation.afterInsert"
-      Project project = image.project;
-      project.countAnnotations++
-  }
-  public def afterDelete()  {
-    println "Annotation.afterDelete"
-      Project project = image.project;
-      project.countAnnotations--
-  }*/
-  /**
-   * Get all terms map with the annotation
-   * @return list of terms
-   */
-  def terms() {
-    return annotationTerm.collect{
-      it.term
-    }
-  }
-
-  def project() {
-    return image?.project
-  }
-
-  String imageFileName() {
-    return this.image?.baseImage?.getFilename()
-  }
-
-  private def getArea() {
-    //TODO: must be compute with zoom level
-    return location.area
-  }
-
-  private def getPerimeter() {
-    //TODO: must be compute with zoom level
-    return location.getLength()
-  }
-
-  private def getCentroid() {
-      if (location.area < 1) return null
-      def centroid = location.getCentroid()
-      def response = [:]
-      response.x = centroid.x
-      response.y = centroid.y
-      return response
-  }
-
-  private def getBoundaries () {
-    /*def metadata = JSON.parse(new URL(image.getMetadataURL()).text)
-    int zoom = Integer.parseInt(metadata.levels)*/
-    Coordinate[] coordinates = location.getEnvelope().getCoordinates()
-    int topLeftX = coordinates[3].x
-    int topLeftY = coordinates[3].y
-    //int topLeftY = Integer.parseInt(metadata.height) - coordinates[3].y
-    int width =  coordinates[1].x - coordinates[0].x
-    int height =  coordinates[3].y - coordinates[0].y
-
-    log.debug "topLeftX :"+ topLeftX + " topLeftY :" + topLeftY + " width :" +  width + " height :" + height
-    return [topLeftX : topLeftX, topLeftY : topLeftY,width : width, height : height]
-  }
-
-  def getCropURL() {
-    def boundaries = getBoundaries()
-    return image.baseImage.getCropURL(boundaries.topLeftX, boundaries.topLeftY, boundaries.width, boundaries.height)
-  }
-
-  def getCropURL(int zoom) {
-    def boundaries = getBoundaries()
-    return image.baseImage.getCropURL(boundaries.topLeftX, boundaries.topLeftY, boundaries.width, boundaries.height, zoom)
-  }
-
-  /**
-   * Create a new Annotation with jsonAnnotation attributes
-   * So, jsonAnnotation must have jsonAnnotation.location, jsonAnnotation.name, ...
-   * @param jsonAnnotation JSON
-   * @return Annotation
-   */
-  static Annotation createFromData(jsonAnnotation) {
-    def annotation = new Annotation()
-    getFromData(annotation,jsonAnnotation)
-  }
-
-  /**
-   * Fill annotation with data attributes
-   * So, jsonAnnotation must have jsonAnnotation.location, jsonAnnotation.name, ...
-   * @param annotation Annotation Source
-   * @param jsonAnnotation JSON
-   * @return annotation with json attributes
-   */
-  static Annotation getFromData(annotation,jsonAnnotation) {
-    annotation.name = jsonAnnotation.name
-    annotation.location = new WKTReader().read(jsonAnnotation.location);
-    //annotation.location = DouglasPeuckerSimplifier.simplify(annotation.location,50)
-    annotation.image = ImageInstance.get(jsonAnnotation.image);
-    annotation.zoomLevel = (!jsonAnnotation.zoomLevel.toString().equals("null"))  ? ((String)jsonAnnotation.zoomLevel).toDouble() : -1
-    annotation.channels =  jsonAnnotation.channels
-    println "jsonAnnotation.user=" + jsonAnnotation.user
-    annotation.user =  User.get(jsonAnnotation.user);
-    println "annotation.user=" + annotation.user
-    annotation.created = (!jsonAnnotation.created.toString().equals("null"))  ? new Date(Long.parseLong(jsonAnnotation.created)) : null
-    annotation.updated = (!jsonAnnotation.updated.toString().equals("null"))  ? new Date(Long.parseLong(jsonAnnotation.updated)) : null
-
-    return annotation;
-  }
-
-
-
-  static void registerMarshaller() {
-    println "Register custom JSON renderer for " + Annotation.class
-    JSON.registerObjectMarshaller(Annotation) {
-      def returnArray = [:]
-      returnArray['class'] = it.class
-      returnArray['id'] = it.id
-      returnArray['name'] = it.name!=""? it.name : "Annotation " + it.id
-      returnArray['location'] = it.location.toString()
-      returnArray['image'] = it.image? it.image.id : null
-      returnArray['imageFilename'] = it.image? it.image.baseImage.filename : null
-      returnArray['zoomLevel'] = it.zoomLevel
-      returnArray['channels'] = it.channels
-      returnArray['user'] = it.user? it.user.id : null
-
-      returnArray['area'] = it.getArea()
-      returnArray['perimeter'] = it.getPerimeter()
-      returnArray['centroid'] = it.getCentroid()
-
-      returnArray['created'] = it.created? it.created.time.toString() : null
-      returnArray['updated'] = it.updated? it.updated.time.toString() : null
-
-      returnArray['term'] = it.terms()
-
-
-      String termList = "";
-      if(it.terms()!=null)
-      {
-        def termName = []
-        it.terms().each{ term ->
-          termName << term.name
+    static mapping = {
+        id generator : "assigned"
+        columns {
+            location type: org.hibernatespatial.GeometryUserType
         }
-        termList = termName.join(',')
-        if(termList.size()>28)
-          termList = termList.substring(0,25) + "..."
-      }
-      returnArray['termList'] =  termList
-
-      returnArray['cropURL'] = UrlApi.getAnnotationCropWithAnnotationId(it.id)
-
-      return returnArray
     }
-  }
+
+    String toString() {return "Annotation " + id}
+
+    /**
+     * If name is empty, fill it by "Annotation $id"
+     */
+    public beforeInsert() {
+        super.beforeInsert()
+        name = name && !name.trim().equals("")? name : "Annotation " + id
+    }
+    /*public afterInsert() {
+      println "Annotation.afterInsert"
+        Project project = image.project;
+        project.countAnnotations++
+    }
+    public def afterDelete()  {
+      println "Annotation.afterDelete"
+        Project project = image.project;
+        project.countAnnotations--
+    }*/
+    /**
+     * Get all terms map with the annotation
+     * @return list of terms
+     */
+    def terms() {
+        return annotationTerm.collect{
+            it.term
+        }
+    }
+
+    def project() {
+        return image?.project
+    }
+
+    String imageFileName() {
+        return this.image?.baseImage?.getFilename()
+    }
+
+    private def getArea() {
+        //TODO: must be compute with zoom level
+        return location.area
+    }
+
+    private def getPerimeter() {
+        //TODO: must be compute with zoom level
+        return location.getLength()
+    }
+
+    private def getCentroid() {
+        if (location.area < 1) return null
+        def centroid = location.getCentroid()
+        def response = [:]
+        response.x = centroid.x
+        response.y = centroid.y
+        return response
+    }
+
+    private def getBoundaries () {
+        /*def metadata = JSON.parse(new URL(image.getMetadataURL()).text)
+     int zoom = Integer.parseInt(metadata.levels)*/
+        Coordinate[] coordinates = location.getEnvelope().getCoordinates()
+        int topLeftX = coordinates[3].x
+        int topLeftY = coordinates[3].y
+        //int topLeftY = Integer.parseInt(metadata.height) - coordinates[3].y
+        int width =  coordinates[1].x - coordinates[0].x
+        int height =  coordinates[3].y - coordinates[0].y
+
+        log.debug "topLeftX :"+ topLeftX + " topLeftY :" + topLeftY + " width :" +  width + " height :" + height
+        return [topLeftX : topLeftX, topLeftY : topLeftY,width : width, height : height]
+    }
+
+    def getCropURL() {
+        def boundaries = getBoundaries()
+        return image.baseImage.getCropURL(boundaries.topLeftX, boundaries.topLeftY, boundaries.width, boundaries.height)
+    }
+
+    def getCropURL(int zoom) {
+        def boundaries = getBoundaries()
+        return image.baseImage.getCropURL(boundaries.topLeftX, boundaries.topLeftY, boundaries.width, boundaries.height, zoom)
+    }
+
+    /**
+     * Create a new Annotation with jsonAnnotation attributes
+     * So, jsonAnnotation must have jsonAnnotation.location, jsonAnnotation.name, ...
+     * @param jsonAnnotation JSON
+     * @return Annotation
+     */
+    static Annotation createFromData(jsonAnnotation) {
+        def annotation = new Annotation()
+        getFromData(annotation,jsonAnnotation)
+    }
+
+    /**
+     * Fill annotation with data attributes
+     * So, jsonAnnotation must have jsonAnnotation.location, jsonAnnotation.name, ...
+     * @param annotation Annotation Source
+     * @param jsonAnnotation JSON
+     * @return annotation with json attributes
+     */
+    static Annotation getFromData(annotation,jsonAnnotation) {
+        annotation.name = jsonAnnotation.name
+        annotation.location = new WKTReader().read(jsonAnnotation.location);
+        //annotation.location = DouglasPeuckerSimplifier.simplify(annotation.location,50)
+        annotation.image = ImageInstance.get(jsonAnnotation.image);
+        annotation.zoomLevel = (!jsonAnnotation.zoomLevel.toString().equals("null"))  ? ((String)jsonAnnotation.zoomLevel).toDouble() : -1
+        annotation.channels =  jsonAnnotation.channels
+        println "jsonAnnotation.user=" + jsonAnnotation.user
+        annotation.user =  User.get(jsonAnnotation.user);
+        println "annotation.user=" + annotation.user
+        annotation.created = (!jsonAnnotation.created.toString().equals("null"))  ? new Date(Long.parseLong(jsonAnnotation.created)) : null
+        annotation.updated = (!jsonAnnotation.updated.toString().equals("null"))  ? new Date(Long.parseLong(jsonAnnotation.updated)) : null
+
+        return annotation;
+    }
+
+
+
+    static void registerMarshaller() {
+        println "Register custom JSON renderer for " + Annotation.class
+        JSON.registerObjectMarshaller(Annotation) {
+            def returnArray = [:]
+            returnArray['class'] = it.class
+            returnArray['id'] = it.id
+            returnArray['name'] = it.name!=""? it.name : "Annotation " + it.id
+            returnArray['location'] = it.location.toString()
+            returnArray['image'] = it.image? it.image.id : null
+            returnArray['imageFilename'] = it.image? it.image.baseImage.filename : null
+            returnArray['zoomLevel'] = it.zoomLevel
+            returnArray['channels'] = it.channels
+            returnArray['user'] = it.user? it.user.id : null
+
+            returnArray['area'] = it.getArea()
+            returnArray['perimeter'] = it.getPerimeter()
+            returnArray['centroid'] = it.getCentroid()
+
+            returnArray['created'] = it.created? it.created.time.toString() : null
+            returnArray['updated'] = it.updated? it.updated.time.toString() : null
+
+            returnArray['term'] = it.terms()
+
+
+            String termList = "";
+            if(it.terms()!=null)
+            {
+                def termName = []
+                it.terms().each{ term ->
+                    termName << term.name
+                }
+                termList = termName.join(',')
+                if(termList.size()>28)
+                    termList = termList.substring(0,25) + "..."
+            }
+            returnArray['termList'] =  termList
+
+            returnArray['cropURL'] = UrlApi.getAnnotationCropWithAnnotationId(it.id)
+
+            return returnArray
+        }
+    }
 
 }
