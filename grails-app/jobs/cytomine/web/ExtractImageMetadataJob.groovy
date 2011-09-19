8b6c9001b2dc44577e7aa96a1b31efcc3d7a63d9
stevben <b.stevens@ulg.ac.be>
2011-09-19 07:46:48 +0000

    Add cron job to retrieve image metadata

---
+++ grails-app/jobs/cytomine/web/ExtractImageMetadataJob.groovy
@@ -0,0 +1,18 @@
+package cytomine.web
+
+import be.cytomine.image.AbstractImage
+
+class ExtractImageMetadataJob {
+
+    def imagePropertiesService
+
+    def startDelay = 5000 //start 5 seconds after server is running
+    def timeout = 1000*60*10 //each 10 minutes
+
+    def execute() {
+        Collection<AbstractImage> abstractImages = AbstractImage.findAllByWidth(null)
+        abstractImages.each { image ->
+            imagePropertiesService.extractUseful(image)
+        }
+    }
+}
