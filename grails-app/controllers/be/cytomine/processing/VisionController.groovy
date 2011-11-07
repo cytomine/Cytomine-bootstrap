991b5b0e546b25a300f29008bb84feef7fb9f5e9
stevben <b.stevens@ulg.ac.be>
2011-11-07 12:00:46 +0000

    Increase robustness of Vision controller

--- grails-app/controllers/be/cytomine/processing/VisionController.groovy
+++ grails-app/controllers/be/cytomine/processing/VisionController.groovy
@@ -11,6 +11,7 @@ import be.cytomine.processing.image.filters.Colour_Deconvolution
 import be.cytomine.processing.image.filters.DynamicThreshold
 import be.cytomine.processing.image.filters.Auto_Threshold
 import ij.plugin.ContrastEnhancer
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 /**
  * Cytomine @ GIGA-ULG
@@ -21,155 +22,174 @@ import ij.plugin.ContrastEnhancer
 class VisionController {
 
     def process = {
-        String imageURL = "http://" + request.queryString.split("url=http://")[1]
+        def split = request.queryString.split("url=http://")
+        String imageURL = ConfigurationHolder.config.grails.serverURL + "/images/notavailable.jpg"
+        if (split.size() > 0) {
+             imageURL = "http://" + split[1]
+        }
+
         println "URL " + params.url
         println "METHOD " + params.method
         println "contrast " + params.contrast
         println "brightness " + params.brightness
 
-        /* Create Buffered Image  From URL */
+        try {
+            /* Create Buffered Image  From URL */
+            BufferedImage bufferedImage = getImageFromURL(imageURL)
+
+            /* Process the BufferedImage */
+            if (params.method == "eosin") {
+                ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
+                Colour_Deconvolution dt = new Colour_Deconvolution()
+                dt.setup(params.url, ip)
+                dt.run(ip.getProcessor())
+                bufferedImage = dt.getResult(1).getBufferedImage()
+            }
+
+            if (params.method == "haematoxylin") {
+                ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
+                Colour_Deconvolution dt = new Colour_Deconvolution()
+                dt.setup(params.url, ip)
+                dt.run(ip.getProcessor())
+                bufferedImage = dt.getResult(0).getBufferedImage()
+            }
+
+            if (params.method == "binary") {
+                ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
+                ImageConverter ic = new ImageConverter(ip)
+                ic.convertToGray8()
+                ip.getProcessor().autoThreshold()
+                bufferedImage = ip.getBufferedImage()
+            }
+
+            if (params.method == "gray") {
+                ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
+                ImageConverter ic = new ImageConverter(ip)
+                ic.convertToGray8()
+                bufferedImage = ip.getBufferedImage()
+            }
+
+            if (params.method == "otsu") {
+                ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
+                ImageConverter ic = new ImageConverter(ip)
+                ic.convertToGray8()
+                Multi_OtsuThreshold dt = new Multi_OtsuThreshold()
+                dt.setup(imageURL, ip)
+                dt.run(ip.getProcessor())
+                bufferedImage = dt.getResult().getBufferedImage()
+            }
+            /* Apply filters */
+            if (params.method=="huang" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "Huang")
+            }
+            if( params.method=="intermodes" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "Intermodes")
+            }
+            if( params.method=="isodata" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "IsoData")
+            }
+            if( params.method=="li" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "Li")
+            }
+            if( params.method=="maxentropy" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "MaxEntropy")
+            }
+            if( params.method=="mean" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "Mean")
+            }
+            if( params.method=="minerror" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "MinError(I)")
+            }
+            if( params.method=="minimum" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "Minimum")
+            }
+            if( params.method=="moments" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "Moments")
+            }
+            if( params.method=="percentile" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "percentile")
+            }
+            if( params.method=="renyientropy" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "RenyiEntropy")
+            }
+            if( params.method=="shanbhag" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "Shanbhag")
+            }
+            if( params.method=="triangle" ) {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "Triangle")
+            }
+            if( params.method=="yen") {
+                bufferedImage = dynBinary(imageURL, bufferedImage, "Yen")
+            }
+
+
+            /* Apply filters */
+            if (Boolean.parseBoolean(params.enhance)) {
+                ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
+                new ContrastEnhancer().stretchHistogram(ip, 0.5)
+                bufferedImage = ip.getBufferedImage()
+            }
+
+            if (Boolean.parseBoolean(params.invert)) {
+                ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
+                ip.getProcessor().invert()
+                bufferedImage = ip.getBufferedImage()
+            }
+
+            if (params.brightness != null && params.contrast != null) {
+                double brightness = Double.parseDouble(params.brightness)
+                double contrast = Double.parseDouble(params.contrast)
+                ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
+                double defaultMin = ip.getDisplayRangeMin()
+                double defaultMax = ip.getDisplayRangeMax()
+                double max = ip.getDisplayRangeMax()
+                double min = ip.getDisplayRangeMin()
+                double range = defaultMax-defaultMin
+                int fullRange = 256
+
+                //BRIGHTNESS
+                def center = defaultMin + (defaultMax-defaultMin)*((range-brightness)/range);
+                double width = max-min;
+
+                min = center - width/2.0;
+                max = center + width/2.0;
+
+                //CONTRAST
+                center = min + (max-min)/2.0;
+                double mid  = fullRange / 2
+                double slope
+                if (contrast <= mid) {
+                    slope = brightness/mid
+                } else {
+                    slope = mid / (fullRange-contrast)
+                }
+                if (slope > 0.0) {
+                    min = center-(0.5*range)/slope
+                    max = center+(0.5*range)/slope;
+                }
+
+                println("MIN/MAX : " + Math.round(min) + "/" + Math.round(max))
+                ip.getProcessor().setMinAndMax(Math.round(min),Math.round(max))
+                bufferedImage = ip.getBufferedImage()
+            }
+
+            /* Write response from BufferedImage */
+            responseImage(bufferedImage)
+
+        } catch (Exception e) {
+            BufferedImage bufferedImage = getImageFromURL(ConfigurationHolder.config.grails.serverURL + "/images/notavailable.jpg")
+            responseImage(bufferedImage)
+        }
+    }
+
+    BufferedImage getImageFromURL(String url)  {
         def out = new ByteArrayOutputStream()
-        out << new URL(imageURL).openStream()
+        out << new URL(url).openStream()
         InputStream inputStream = new ByteArrayInputStream(out.toByteArray());
-        BufferedImage bufferedImage = ImageIO.read(inputStream);
+        return ImageIO.read(inputStream);
+    }
 
-        /* Process the BufferedImage */
-        if (params.method == "eosin") {
-            ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
-            Colour_Deconvolution dt = new Colour_Deconvolution()
-            dt.setup(params.url, ip)
-            dt.run(ip.getProcessor())
-            bufferedImage = dt.getResult(1).getBufferedImage()
-        }
-
-        if (params.method == "haematoxylin") {
-            ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
-            Colour_Deconvolution dt = new Colour_Deconvolution()
-            dt.setup(params.url, ip)
-            dt.run(ip.getProcessor())
-            bufferedImage = dt.getResult(0).getBufferedImage()
-        }
-
-        if (params.method == "binary") {
-            ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
-            ImageConverter ic = new ImageConverter(ip)
-            ic.convertToGray8()
-            ip.getProcessor().autoThreshold()
-            bufferedImage = ip.getBufferedImage()
-        }
-
-        if (params.method == "gray") {
-            ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
-            ImageConverter ic = new ImageConverter(ip)
-            ic.convertToGray8()
-            bufferedImage = ip.getBufferedImage()
-        }
-
-        if (params.method == "otsu") {
-            ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
-            ImageConverter ic = new ImageConverter(ip)
-            ic.convertToGray8()
-            Multi_OtsuThreshold dt = new Multi_OtsuThreshold()
-            dt.setup(imageURL, ip)
-            dt.run(ip.getProcessor())
-            bufferedImage = dt.getResult().getBufferedImage()
-        }
-        /* Apply filters */
-        if (params.method=="huang" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "Huang")
-        }
-        if( params.method=="intermodes" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "Intermodes")
-        }
-        if( params.method=="isodata" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "IsoData")
-        }
-        if( params.method=="li" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "Li")
-        }
-        if( params.method=="maxentropy" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "MaxEntropy")
-        }
-        if( params.method=="mean" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "Mean")
-        }
-        if( params.method=="minerror" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "MinError(I)")
-        }
-        if( params.method=="minimum" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "Minimum")
-        }
-        if( params.method=="moments" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "Moments")
-        }
-        if( params.method=="percentile" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "percentile")
-        }
-        if( params.method=="renyientropy" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "RenyiEntropy")
-        }
-        if( params.method=="shanbhag" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "Shanbhag")
-        }
-        if( params.method=="triangle" ) {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "Triangle")
-        }
-        if( params.method=="yen") {
-            bufferedImage = dynBinary(imageURL, bufferedImage, "Yen")
-        }
-
-
-        /* Apply filters */
-        if (Boolean.parseBoolean(params.enhance)) {
-            ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
-            new ContrastEnhancer().stretchHistogram(ip, 0.5)
-            bufferedImage = ip.getBufferedImage()
-        }
-
-        if (Boolean.parseBoolean(params.invert)) {
-            ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
-            ip.getProcessor().invert()
-            bufferedImage = ip.getBufferedImage()
-        }
-
-        if (params.brightness != null && params.contrast != null) {
-            double brightness = Double.parseDouble(params.brightness)
-            double contrast = Double.parseDouble(params.contrast)
-            ImagePlus ip = new ImagePlus(imageURL,bufferedImage)
-            double defaultMin = ip.getDisplayRangeMin()
-            double defaultMax = ip.getDisplayRangeMax()
-            double max = ip.getDisplayRangeMax()
-            double min = ip.getDisplayRangeMin()
-            double range = defaultMax-defaultMin
-            int fullRange = 256
-
-            //BRIGHTNESS
-            def center = defaultMin + (defaultMax-defaultMin)*((range-brightness)/range);
-            double width = max-min;
-
-            min = center - width/2.0;
-            max = center + width/2.0;
-
-            //CONTRAST
-            center = min + (max-min)/2.0;
-            double mid  = fullRange / 2
-            double slope
-            if (contrast <= mid) {
-                slope = brightness/mid
-            } else {
-                slope = mid / (fullRange-contrast)
-            }
-            if (slope > 0.0) {
-                min = center-(0.5*range)/slope
-                max = center+(0.5*range)/slope;
-            }
-
-            println("MIN/MAX : " + Math.round(min) + "/" + Math.round(max))
-            ip.getProcessor().setMinAndMax(Math.round(min),Math.round(max))
-            bufferedImage = ip.getBufferedImage()
-        }
-
-        /* Write response from BufferedImage */
+    def responseImage(BufferedImage bufferedImage) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ImageIO.write(bufferedImage, "jpg", baos);
         byte[] bytesOut = baos.toByteArray();
@@ -185,13 +205,11 @@ class VisionController {
                 else {
                     response.contentType = "image/jpeg";
                     response.getOutputStream() << bytesOut
-                    out.flush()
                     response.getOutputStream().flush()
                 }
             }
         }
     }
-
 
     def dynBinary (String url, BufferedImage bufferedImage, String method) {
         ImagePlus ip = new ImagePlus(url,bufferedImage)
--- web-app/application/views/ApplicationView.js
+++ web-app/application/views/ApplicationView.js
@@ -1,194 +1,194 @@
 var ApplicationView = Backbone.View.extend({
 
-   tagName : "div",
-   className : "layout",
-   components : {},
-   events: {
-      "click #undo":          "undo",
-      "click #redo":          "redo"
-   },
+    tagName : "div",
+    className : "layout",
+    components : {},
+    events: {
+        "click #undo":          "undo",
+        "click #redo":          "redo"
+    },
 
-   /**
-    *  UNDO the last command
-    */
-   undo : function () {
-      window.app.controllers.command.undo();
-   },
+    /**
+     *  UNDO the last command
+     */
+    undo : function () {
+        window.app.controllers.command.undo();
+    },
 
-   /**
-    * REDO the last command
-    */
-   redo : function () {
-      window.app.controllers.command.redo();
-   },
+    /**
+     * REDO the last command
+     */
+    redo : function () {
+        window.app.controllers.command.redo();
+    },
 
-   /**
-    * ApplicationView constructor. Call the initialization of its components
-    * @param options
-    */
-   initialize: function(options) {
+    /**
+     * ApplicationView constructor. Call the initialization of its components
+     * @param options
+     */
+    initialize: function(options) {
 
-   },
-   /**
-    * Render the html into the DOM element associated to the view
-    * @param tpl
-    */
-   doLayout: function(tpl, renderCallback) {
-      $(this.el).html(_.template(tpl, {}));
-      _.each(this.components, function (component) {
-         component.render();
-      });
+    },
+    /**
+     * Render the html into the DOM element associated to the view
+     * @param tpl
+     */
+    doLayout: function(tpl, renderCallback) {
+        $(this.el).html(_.template(tpl, {}));
+        _.each(this.components, function (component) {
+            component.render();
+        });
 
-      renderCallback.call();
-      //$('#switcher').themeswitcher();
+        renderCallback.call();
+        //$('#switcher').themeswitcher();
 
-      return this;
-   },
-   /**
-    * Grab the layout and call ask for render
-    */
-   render : function(renderCallback) {
-      this.initComponents();
-      var self = this;
-      require([
-         "text!application/templates/BaseLayout.tpl.html"
-      ],
-          function(tpl) {
-             self.doLayout(tpl, renderCallback);
-          });
-      return this;
-   },
-   initUserMenu : function () {
+        return this;
+    },
+    /**
+     * Grab the layout and call ask for render
+     */
+    render : function(renderCallback) {
+        this.initComponents();
+        var self = this;
+        require([
+            "text!application/templates/BaseLayout.tpl.html"
+        ],
+                function(tpl) {
+                    self.doLayout(tpl, renderCallback);
+                });
+        return this;
+    },
+    initUserMenu : function () {
 
-     //Init user menu
-      require([
-         "text!application/templates/MenuDropDown.tpl.html"
-      ],function(tpl) {
-         $("#menu-right").append(tpl);
-         $("#logout").click(function() { window.app.controllers.auth.logout();return false; });
-         $("#loggedUser").html(window.app.status.user.model.prettyName());
-      });
+        //Init user menu
+        require([
+            "text!application/templates/MenuDropDown.tpl.html"
+        ],function(tpl) {
+            $("#menu-right").append(tpl);
+            $("#logout").click(function() { window.app.controllers.auth.logout();return false; });
+            $("#loggedUser").html(window.app.status.user.model.prettyName());
+        });
 
-   },
-   /**
-    * Initialize the components of the application
-    */
-   initComponents : function() {
-      var self = this;
-      require([
-         "text!application/templates/upload/UploadComponent.tpl.html",
-         "text!application/templates/WarehouseComponent.tpl.html",
-         "text!application/templates/explorer/ExplorerComponent.tpl.html",
-         "text!application/templates/AdminComponent.tpl.html"
-      ],
-          function(uploadTpl, warehouseTpl, explorerTpl, adminTpl) {
-             self.components.upload = new Component({
-                el : "#content",
-                template : _.template(uploadTpl, {}),
-                buttonAttr : {
-                   elButton : "upload-button",
-                   buttonText : "Upload",
-                   buttonWrapper : "#menu",
-                   dataContent : "Send your data !",
-                   dataTitle : "Upload",
-                   icon : "ui-icon-circle-arrow-s",
-                   route : "#upload"
-                },
-                divId : "upload"
-             });
-             self.components.warehouse = new Component({
-                el : "#content",
-                template : _.template(warehouseTpl, {}),
-                buttonAttr : {
-                   elButton : "warehouse-button",
-                   buttonText : "Organize",
-                   buttonWrapper : "#menu",
-                   dataContent : "Organize your projects, images, etc...",
-                   dataTitle : "Organize",
-                   icon : "ui-icon-wrench",
-                   route : "#project"
-                },
-                divId : "warehouse"
-             });
-             self.components.explorer = new Component({
-                el : "#content",
-                template : _.template(explorerTpl, {}),
-                buttonAttr : {
-                   elButton : "explorer-button",
-                   buttonText : "Explore",
-                   buttonWrapper : "#menu",
-                   dataContent : "View your data",
-                   dataTitle : "Explore",
-                   icon : "ui-icon-image",
-                   route : "#explorer"
-                },
-                divId : "explorer",
-                activate: function () {
-                   if (window.app.status.currentProject == undefined)
-                      $("#explorer > .noProject").show();
-                   else
-                      $("#explorer > .noProject").hide();
-                   $("#" + this.divId).show();
-                   $("#" + this.buttonAttr.elButton).addClass("ui-state-disabled");
-                }
-             });
-             /*self.components.admin = new Component({
-                el : "#content",
-                template : _.template(adminTpl, {}),
-                buttonAttr : {
-                   elButton : "admin-button",
-                   buttonText : "Admin",
-                   buttonWrapper : "#menu",
-                   icon : "ui-icon-wrench",
-                   route : "#admin/users"
-                },
-                divId : "admin"
-             });*/
+    },
+    /**
+     * Initialize the components of the application
+     */
+    initComponents : function() {
+        var self = this;
+        require([
+            "text!application/templates/upload/UploadComponent.tpl.html",
+            "text!application/templates/WarehouseComponent.tpl.html",
+            "text!application/templates/explorer/ExplorerComponent.tpl.html",
+            "text!application/templates/AdminComponent.tpl.html"
+        ],
+                function(uploadTpl, warehouseTpl, explorerTpl, adminTpl) {
+                    self.components.upload = new Component({
+                        el : "#content",
+                        template : _.template(uploadTpl, {}),
+                        buttonAttr : {
+                            elButton : "upload-button",
+                            buttonText : "Upload",
+                            buttonWrapper : "#menu",
+                            dataContent : "Send your data !",
+                            dataTitle : "Upload",
+                            icon : "ui-icon-circle-arrow-s",
+                            route : "#upload"
+                        },
+                        divId : "upload"
+                    });
+                    self.components.warehouse = new Component({
+                        el : "#content",
+                        template : _.template(warehouseTpl, {}),
+                        buttonAttr : {
+                            elButton : "warehouse-button",
+                            buttonText : "Organize",
+                            buttonWrapper : "#menu",
+                            dataContent : "Organize your projects, images, etc...",
+                            dataTitle : "Organize",
+                            icon : "ui-icon-wrench",
+                            route : "#project"
+                        },
+                        divId : "warehouse"
+                    });
+                    self.components.explorer = new Component({
+                        el : "#content",
+                        template : _.template(explorerTpl, {}),
+                        buttonAttr : {
+                            elButton : "explorer-button",
+                            buttonText : "Explore",
+                            buttonWrapper : "#menu",
+                            dataContent : "View your data",
+                            dataTitle : "Explore",
+                            icon : "ui-icon-image",
+                            route : "#explorer"
+                        },
+                        divId : "explorer",
+                        activate: function () {
+                            if (window.app.status.currentProject == undefined)
+                                $("#explorer > .noProject").show();
+                            else
+                                $("#explorer > .noProject").hide();
+                            $("#" + this.divId).show();
+                            $("#" + this.buttonAttr.elButton).addClass("ui-state-disabled");
+                        }
+                    });
+                    /*self.components.admin = new Component({
+                     el : "#content",
+                     template : _.template(adminTpl, {}),
+                     buttonAttr : {
+                     elButton : "admin-button",
+                     buttonText : "Admin",
+                     buttonWrapper : "#menu",
+                     icon : "ui-icon-wrench",
+                     route : "#admin/users"
+                     },
+                     divId : "admin"
+                     });*/
 
-             /*self.components.logout = new Component({
-                el : "#content",
-                template : "",
-                buttonAttr : {
-                   elButton : "user-button",
-                   buttonText :,
-                   buttonWrapper : "#menu",
-                   dataContent : "we have to delete this popover for logout",
-                   dataTitle : "huhu",
-                   icon : "ui-icon-power",
-                   route : "#",
-                   click :
-                },
-                divId : "logout"
-             });*/
-          });
-   },
-   /**
-    * Show a component
-    * @param Component the reference to the component
-    */
-   showComponent : function (component) {
-      _.each(this.components, function (c) {
-         if (c != component) c.deactivate();
-      });
-      $("#app").show();
-      component.activate();
-   }
+                    /*self.components.logout = new Component({
+                     el : "#content",
+                     template : "",
+                     buttonAttr : {
+                     elButton : "user-button",
+                     buttonText :,
+                     buttonWrapper : "#menu",
+                     dataContent : "we have to delete this popover for logout",
+                     dataTitle : "huhu",
+                     icon : "ui-icon-power",
+                     route : "#",
+                     click :
+                     },
+                     divId : "logout"
+                     });*/
+                });
+    },
+    /**
+     * Show a component
+     * @param Component the reference to the component
+     */
+    showComponent : function (component) {
+        _.each(this.components, function (c) {
+            if (c != component) c.deactivate();
+        });
+        $("#app").show();
+        component.activate();
+    }
 });
 
 ApplicationView.prototype.message =  function(title, message, type) {
-   type = type || 'info';
-   if(message!=undefined) {
-      message.responseText && (message = message.responseText);
-   }
+    type = type || 'info';
+    if(message!=undefined) {
+        message.responseText && (message = message.responseText);
+    }
 
-   var tpl = '<div style="min-width: 200px" id="alert{{timestamp}}" class="alert-message {{type}} fade in" data-alert="alert"><a class="close" href="#">×</a><p><strong>{{alert}}</strong> {{message}}</p></div>';
-   var timestamp = new Date().getTime();
-   $("#alerts").append(_.template(tpl, { alert : title, message : message, timestamp : timestamp, type : type}));
-   $("#alert"+timestamp).alert();
-   setTimeout(function(){
-      $("#alert"+timestamp).alert('close');
-      $("#alert"+timestamp).remove();
-   }, 3000);
+    var tpl = '<div style="min-width: 200px" id="alert{{timestamp}}" class="alert-message {{type}} fade in" data-alert="alert"><a class="close" href="#">×</a><p><strong>{{alert}}</strong> {{message}}</p></div>';
+    var timestamp = new Date().getTime();
+    $("#alerts").append(_.template(tpl, { alert : title, message : message, timestamp : timestamp, type : type}));
+    $("#alert"+timestamp).alert();
+    setTimeout(function(){
+        $("#alert"+timestamp).alert('close');
+        $("#alert"+timestamp).remove();
+    }, 3000);
 
 }
 
