b9d2c355841e385f2a4f94b46e38fde5b01c86ab
stevben <b.stevens@ulg.ac.be>
2012-03-20 11:10:48 +0000

    Add JODA Time library

--- grails-app/domain/be/cytomine/image/UploadedFile.groovy
+++ grails-app/domain/be/cytomine/image/UploadedFile.groovy
@@ -1,11 +1,16 @@
 package be.cytomine.image
 
 import be.cytomine.security.SecUser
+import be.cytomine.project.Project
 
 class UploadedFile {
 
     SecUser user
+    Project project
+    String filename
+    String originalFilename
 
     static constraints = {
+
     }
 }
