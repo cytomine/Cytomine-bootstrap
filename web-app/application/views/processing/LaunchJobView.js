f770bdb27ed5bc8153057fc1f8cd6c5469675c2a
loic911 <lrollus@ulg.ac.be>
2012-03-20 14:26:51 +0000

    Generic backbones domain support for parameter

--- web-app/application/views/processing/LaunchJobView.js
+++ web-app/application/views/processing/LaunchJobView.js
@@ -56,9 +56,10 @@ var LaunchJobView = Backbone.View.extend({
         $("#jobTitle").append("<h3>Run job from " + self.software.get('name') + " on project " + self.project.get('name') +" </h3>");
 
          _.each(self.software.get('parameters'), function (param) {
-            if(param.name.toLowerCase() != "privatekey" && param.name.toLowerCase() != "publickey")
+            if(param.name.toLowerCase() != "privatekey" && param.name.toLowerCase() != "publickey") {
                self.params.push(param);
                self.paramsViews.push(self.getParamView(param));
+            }
             });
 
         self.printSoftwareParams();
@@ -69,6 +70,9 @@ var LaunchJobView = Backbone.View.extend({
         if(param.type=="Number") return new InputNumberView({param:param});
         if(param.type=="Boolean") return new InputBooleanView({param:param});
         if(param.type=="List") return new InputListView({param:param});
+        if(param.type=="ListProject") return new InputListDomainView({param:param, multiple:true, collection: window.app.models.projects, printAttribut:"name"});
+        if(param.type=="Project") return new InputListDomainView({param:param,multiple:false, collection:  window.app.models.projects, printAttribut:"name"});
+
 
         else return new InputTextView({param:param});
     },
@@ -101,145 +105,9 @@ var LaunchJobView = Backbone.View.extend({
                 { "sWidth": "25%", "aTargets": [ 2 ] }
              ]
         });
-
-//        $('#launchJobParamsTable').find('input').keyup(function() {
-//             self.checkEntryValidation();
-//        });
-//
-//        $('#launchJobParamsTable').find('.icon-plus-sign').click(function() {
-//            console.log("Add entry");
-//            var value = $(this).parent().find("input").val();
-//            $(this).parent().find("select").append('<option value="'+value+'">'+value+'</option>');
-//            $(this).parent().find("input").val("");
-//            $(this).parent().find("select").val(value);
-//            self.checkEntryValidation();
-//        });
-//
-//        $('#launchJobParamsTable').find('.icon-minus-sign').click(function() {
-//
-//            var value = $(this).parent().find("select").val();
-//            console.log("delete entry:"+value);
-//            console.log("delete entry:"+$(this).parent().find("select").find('[value="'+value+'"]').length);
-//            $(this).parent().find("select").find('[value="'+value+'"]').remove();
-//            self.checkEntryValidation();
-//        });
-//
-//        var fn = function(numChecked, numTotal, checkedItem) {
-//            var selectTitle = [];
-//            $(checkedItem).each(function() {
-//               selectTitle.push($(this).attr("title"));
-//            });
-//            var selectText = selectTitle.join(", ");
-//            selectText = selectText.substring(0,Math.min(15,selectText.length));
-//            return numChecked + " selected: " + selectText + "...";
-//        };
-//
-//        var fn2 = function(numChecked, numTotal, checkedItem) {
-//            if(numChecked==0) return "0 selected";
-//            var selectTitle = [];
-//            $(checkedItem).each(function() {
-//               selectTitle.push($(this).attr("title"));
-//            });
-//            var selectText = selectTitle.join(", ");
-//            selectText = selectText.substring(0,Math.min(15,selectText.length));
-//            return selectText;
-//        };
-//
-//
-//        $('#launchJobParamsTable').find(".projectListN").multiselect({'autoOpen':false,'height':200,'selectedText':fn}).multiselectfilter();
-//        $('#launchJobParamsTable').find(".projectList1").multiselect({'autoOpen':false,'height':200,'multiple':false,'selectedText':fn2}).multiselectfilter();
-//        $('#launchJobParamsTable').find(".ui-multiselect-menu").find("span").css("display","inline");
-//        $('#launchJobParamsTable').find(".ui-multiselect-menu").find(".ui-multiselect-header").find("li").css("display","inline");
-//        $('#launchJobParamsTable').find(".ui-multiselect-menu").find(".ui-multiselect-header").find("li").eq(0).css("float","left");
-//        $('#launchJobParamsTable').find(".ui-multiselect-menu").find(".ui-multiselect-header").find("li").eq(1).css("float","right");
-//        //$('#launchJobParamsTable').find(".ui-multiselect-menu").find("label").css("float","left");
-//        $('#launchJobParamsTable').find(".ui-multiselect-menu").find("li").css("display","block");
-//        console.log("############################################################");
-//        console.log($('#launchJobParamsTable').find(".ui-multiselect-menu").find("span").length);
-//
-//
-//            $('#launchJobParamsTable').find("ul.ui-multiselect-checkboxes").css('overflow-y','scroll');
-//        $('#launchJobParamsTable').find("ul.ui-multiselect-checkboxes").css('overflow-x','hidden');
-//        $('#launchJobParamsTable').find('button.ui-multiselect').click();
-//        $('#launchJobParamsTable').find('button.ui-multiselect').click();
     },
-//    getEntryByType : function(param) {
-//        var self = this;
-//        console.log("defaultValue=" + param.defaultParamValue);
-//        var classRequier = ""
-//        if(param.required)  classRequier = 'border-style: solid; border-width: 2px;'
-//        else classRequier = 'border-style: dotted;border-width: 2px;'
-//
-//        //TODO: décoder les variables (style $currentProject)
-//
-//        if (param.type == "String") {
-//            return '<div class="control-group success"><div class="controls"><input type="text" class="span3" value="' + self.getDefaultValue(param) + '" style="text-align:center;'+classRequier+'"></div></div>';
-//        }
-//        if (param.type == "Number") {
-//            return '<div class="control-group success"><div class="controls"><input type="text" class="span3" value="' + self.getDefaultValue(param) + '" style="text-align:center;'+classRequier+'" ></div></div>';
-//        }
-//        if (param.type == "Boolean") {
-//            return '<input type="checkbox" class="span3" ' + self.getDefaultValue(param) + ' />';
-//        }
-//        //
-//        if(param.type == "List") {
-//            var defaultValues = [self.getDefaultValue(param)];
-//            var valueStr = '<div class="control-group success"><div class="controls"><input type="text" class="span3" value="' + "" + '" style="text-align:center;'+classRequier+'"><i class="icon-plus-sign"></i><select>';
-//            _.each(defaultValues,function(value) {
-//                valueStr = valueStr + '<option value="'+value+'">'+value+'</option>';
-//            });
-//            valueStr = valueStr + '</select><i class="icon-minus-sign"></i></div></div>';
-//            return valueStr;
-//        }
-//
-//        if(param.type == "ListProject") {
-//
-//               return '<select id="countries" class="projectListN" multiple="multiple" name="countries[]"></select>';
-//
-//
-//                  '<option value="AFG">Afghanistan</option>' +
-//                  '<option value="ALB">Albania</option>' +
-//                  '<option value="DZA">Algeria</option>' +
-//                  '<option value="AND">Andorra</option>' +
-//                  '<option value="ARG">Argentina</option>' +
-//                  '<option value="ARM">Armenia</option>'+
-//                  '<option value="ABW">Aruba</option>' +
-//                  '<option value="AUS">Australia</option>' +
-//                       '<option value="AUS">AZ1</option>' +
-//                       '<option value="AUS">AZ2</option>' +
-//                  '<option value="AUT" selected="selected">Austria</option>'+ '</select>';
-//
-//                     ///if(window.app.models.projects==undefined || window.app.models.projects==null) {
-//
-//         //}
-//
-//
-//        }
-//
-//        if(param.type == "Project") {
-//               return '<select id="countries" class="projectList1" multiple="multiple" name="countries[]">' +
-//                  '<option value="AFG">Afghanistan</option>' +
-//                  '<option value="ALB">Albania</option>' +
-//                  '<option value="DZA">Algeria</option>' +
-//                  '<option value="AND">Andorra</option>' +
-//                  '<option value="ARG">Argentina</option>' +
-//                  '<option value="ARM">Armenia</option>'+
-//                  '<option value="ABW">Aruba</option>' +
-//                  '<option value="AUS">Australia</option>' +
-//                       '<option value="AUS">AZ1</option>' +
-//                       '<option value="AUS">AZ2</option>' +
-//                  '<option value="AUT">Austria</option>'+ '</select>';
-//        }
-//
-//
-//    },
-//    getDefaultValue : function(param) {
-//        var self = this;
-//        if(param.type=="Boolean" && param.defaultParamValue!=undefined && param.defaultParamValue.toLowerCase()=="true") return 'checked="checked"';
-//        if(param.defaultParamValue=="$currentProject") return self.project.id;
-//        if(param.defaultParamValue=="$cytomineHost") return window.location.protocol + "//" + window.location.host;
-//        return param.defaultParamValue;
-//    },
+
+
     createJobFromParam :function() {
         var self = this;
         //retrieve an array of param
@@ -489,11 +357,162 @@ var InputListView = Backbone.View.extend( {
     }
 });
 
+var InputListDomainView = Backbone.View.extend( {
+    param : null,
+    parent : null,
+    trElem : null,
+    multiple : true,
+    collection : null,
+    printAttribut : null,
+    initialize: function(options) {
+        this.param = options.param;
+        this.parent = options.parent;
+        this.multiple = options.multiple;
+        this.collection = options.collection;
+        this.printAttribut = options.printAttribut;
+    },
+    addRow : function(tbody) {
+        var self = this;
+         tbody.append('<tr id="'+self.param.id+'"><td style="text-align:left;">'+self.param.name+ '</td><td id="'+self.param.id+'" style="text-align:center;"></td><td style="text-align:center;"><span class="errorMessage label label-important hidden"></span></td></tr>');
+        self.trElem = $('tr#'+self.param.id);
+        console.log("*****************************");
+        console.log(self.collection);
+        console.log(self.collection.at(0));
+        if(self.collection==undefined || (self.collection.length>0 && self.collection.at(0).id==undefined)) {
+            self.collection.fetch({
+                success : function(collection, response) {
+                    console.log("*****************************");
+                    console.log(collection);
+                    self.collection=collection;
+                    self.addHtmlElem();
+                }
+            });
+        } else self.addHtmlElem();
+    },
+    addHtmlElem : function() {
+        var self = this;
+                self.trElem.find("td#"+self.param.id).append(self.getHtmlElem());
 
 
+                var fnSelectionView = null;
+
+                if(self.multiple) {
+                    fnSelectionView = function(numChecked, numTotal, checkedItem) {
+                    var selectTitle = [];
+                    $(checkedItem).each(function() {
+                       selectTitle.push($(this).attr("title"));
+                    });
+                    var selectText = selectTitle.join(", ");
+                    selectText = selectText.substring(0,Math.min(15,selectText.length));
+                    return numChecked + " selected: " + selectText + "...";
+                    }
+                } else {
+                    fnSelectionView = function(numChecked, numTotal, checkedItem) {
+                    if(numChecked==0) return "0 selected";
+                    var selectTitle = [];
+                    $(checkedItem).each(function() {
+                       selectTitle.push($(this).attr("title"));
+                    });
+                    var selectText = selectTitle.join(", ");
+                    selectText = selectText.substring(0,Math.min(15,selectText.length));
+                    return selectText;
+                    };
+                }
+
+                self.trElem.find(".domainList").multiselect({'autoOpen':false,minWidth:300,'height':200,'multiple':self.multiple,'selectedText':fnSelectionView}).multiselectfilter();
+                //put header menu option on the same line
+                self.trElem.find(".ui-multiselect-menu").find("span").css("display","inline");
+                self.trElem.find(".ui-multiselect-menu").find(".ui-multiselect-header").find("li").css("display","inline");
+                //put check all on left and deselect all on right
+                self.trElem.find(".ui-multiselect-menu").find(".ui-multiselect-header").find("li").eq(0).css("float","left");
+                self.trElem.find(".ui-multiselect-menu").find(".ui-multiselect-header").find("li").eq(1).css("float","right");
+                self.trElem.find(".ui-multiselect-menu").find("li").css("display","block");
+                //print scroll only vertical
+                self.trElem.find("ul.ui-multiselect-checkboxes").css('overflow-y','scroll');
+                self.trElem.find("ul.ui-multiselect-checkboxes").css('overflow-x','hidden');
+                //autoOpen:false doesn't work, so click to hide open multiselect
+                self.trElem.find('button.ui-multiselect').click();
+                self.trElem.find('button.ui-multiselect').click();
+
+                self.checkEntryValidation();
 
 
+                self.trElem.find(".domainList").bind("multiselectclick", function(event, ui){
+                        self.checkEntryValidationWithAllValue(ui.value,ui.checked);
+                });
 
+    },
+    getDefaultValue : function() {
+        var self = this;
+        var split = self.param.defaultParamValue.split(",");
+        var values = [];
+        console.log(split);
+        _.each(split, function(s) {
+            console.log(s);
+           values.push(InputView.getDefaultValueWithVariable(s));
+        });
+        console.log(values);
+        return values;
+    },
+    getHtmlElem : function() {
+        var self = this;
+        var classRequier = "";
+       //mark default value as selected:
+        var defaultValues = self.getDefaultValue();
+        if(self.param.required)  classRequier = 'border-style: solid; border-width: 2px;'
+        else classRequier = 'border-style: dotted;border-width: 2px;'
+            var valueStr = '<select class="domainList" multiple="multiple">';
+            self.collection.each(function(value) {
+                var selClass = "";
+                _.each(defaultValues,function(def) {
+                    if(def==value.id) {
+                        selClass = 'selected="selected"';
+                    }
+                });
+                valueStr = valueStr + '<option '+ selClass +' value="'+value.id+'">'+value.get(self.printAttribut)+'</option>';
+            });
+            valueStr = valueStr + '</select>';
+            return valueStr;
+    },
+    checkEntryValidationWithAllValue : function(value,checked) {
+        var self = this;
+        console.log("checkEntryValidation");
+
+        var values = self.getValue();
+        var length = values.length;
+        if(checked) length++;
+        else length--;
+        console.log(self.param.required + "|" + values);
+        if(self.param.required && length==0) self.changeStyle(self.trElem,false,"Field require");
+        else self.changeStyle(self.trElem,true,"");
+
+    },
+    checkEntryValidation : function() {
+        var self = this;
+        console.log("checkEntryValidation");
+        var values = self.getValue();
+        console.log(self.param.required + "|" + values.length);
+        if(self.param.required && values.length==0) self.changeStyle(self.trElem,false,"Field require");
+        else self.changeStyle(self.trElem,true,"");
+    },
+    getValue : function() {
+        var self = this;
+        var values = self.trElem.find(".domainList").val();
+        if(values==undefined) return [];
+        else return values;
+    },
+    changeStyle : function(elem, success, message) {
+        var labelElem = elem.find('.errorMessage');
+        if(success) {
+            labelElem.addClass("hidden");
+            labelElem.text("");
+        }else {
+            labelElem.removeClass("hidden");
+            labelElem.text("");
+            labelElem.text(message);
+        }
+    }
+});
 
 var InputView = {
     changeStyle : function(elem, success, message) {
