9f43888e702225e4bddae1dced023ddc8a7ce6e3
stevben <b.stevens@ulg.ac.be>
2012-12-12 22:20:25 +0000

    add processing server domain

---
+++ grails-app/domain/be/cytomine/image/server/ProcessingServer.groovy
@@ -0,0 +1,12 @@
+package be.cytomine.image.server
+
+import be.cytomine.CytomineDomain
+
+class ProcessingServer  extends CytomineDomain {
+
+    String url
+
+    static constraints = {
+        url nullable: false
+    }
+}
