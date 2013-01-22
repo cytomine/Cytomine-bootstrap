105e39e9ff84bc0ffa9c897b1585ad454dbd8d95
stevben <b.stevens@ulg.ac.be>
2013-01-22 10:28:43 +0000

    add plugin dependencies in BuildConfig
    Former-commit-id: 621f34648ff0673c9d64bafbad224e745fc4668f

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,6 +10,8 @@ grails.project.dependency.resolution = {
         // excludes 'ehcache'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
         grailsPlugins()
@@ -25,22 +27,47 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
         mavenLocal()
-      		mavenCentral()
+        mavenCentral()
 
-      		// For Geb snapshot
-      		mavenRepo "https://nexus.codehaus.org/content/repositories/snapshots"
+        // For Geb snapshot
+        mavenRepo "https://nexus.codehaus.org/content/repositories/snapshots"
     }
     dependencies {
-    }
-    plugins {
 
     }
+    plugins {
+        // Uncomment these (or add new ones) to enable additional resources capabilities
+        //runtime ":zipped-resources:1.0"
+        //runtime ":cached-resources:1.0"
+        //runtime ":yui-minify-resources:0.1.4"
+        build ":tomcat:$grailsVersion"
+
+        runtime ":hibernate:$grailsVersion"
+        runtime ':spring-security-core:1.2.7.3'
+        runtime ':spring-security-acl:1.1.1'
+        runtime ':spring-security-appinfo:1.0'
+        runtime ':background-thread:1.6'
+        runtime ':export:1.5'
+        runtime ':fields:1.3'
+        runtime ':functional-test:2.0.RC1'
+        runtime ':twitter-bootstrap:2.0.4'
+        runtime ':quartz:1.0-RC5'
+        runtime ':quartz-monitor:0.3-RC1'
+        runtime ':rest:0.7'
+        runtime ':cache:1.0.1'
+        runtime ':database-migration:1.2.1'
+        runtime ":resources:1.2.RC2"
+        runtime ':jquery:1.8.3'
+
+        test ':code-coverage:1.2.5'
+        test ':selenium-rc:1.0.2'
+    }
 }
-coverage {
-	exclusions = [
-            "**/be/cytomine/data/**",
-            "**/be/cytomine/processing/job/**",
-            "**/be/cytomine/processing/image/filters/**"
-    ]
-}
+//coverage {
+//	exclusions = [
+//            "**/be/cytomine/data/**",
+//            "**/be/cytomine/processing/job/**",
+//            "**/be/cytomine/processing/image/filters/**"
+//    ]
+//}
 
