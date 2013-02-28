81bbbb4052c239d8c841a79657de949a1039180c
loic911 <lrollus@ulg.ac.be>
2013-02-28 06:48:46 +0000

    try to fix test
    Former-commit-id: ec96237ada383281fe59d7e2ca6f597b7b2716c3

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -101,7 +101,7 @@ class BootStrap {
             log.info inputArgs.get(i)
         }
 
-        createUsers()
+//        createUsers()
 
         /* Fill data just in test environment*/
         if (GrailsUtil.environment == BootStrap.test) {
@@ -469,14 +469,13 @@ class BootStrap {
 
     def createUsers() {
 
-//        def usersSamples = [
-//                [username : 'rmaree', firstname : 'Raphaël', lastname : 'Marée', email : 'rmaree@ulg.ac.be', group : [[name : "GIGA"]], password : 'rM$2011', color : "#FF0000", roles : ["ROLE_USER", "ROLE_ADMIN"]],
-//                [username : 'lrollus', firstname : 'Loïc', lastname : 'Rollus', email : 'lrollus@ulg.ac.be', group : [[name : "GIGA"]], password : 'lR$2011', color : "#00FF00", roles : ["ROLE_USER", "ROLE_ADMIN"]],
-//                [username : 'stevben', firstname : 'Benjamin', lastname : 'Stévens', email : 'bstevens@ulg.ac.be', group : [[name : "GIGA"]], password : 'sB$2011', color : "#0000FF",roles : ["ROLE_USER", "ROLE_ADMIN"]]
-//        ]
         def usersSamples = [
+                [username : 'rmaree', firstname : 'Raphaël', lastname : 'Marée', email : 'rmaree@ulg.ac.be', group : [[name : "GIGA"]], password : 'rM$2011', color : "#FF0000", roles : ["ROLE_USER", "ROLE_ADMIN"]],
+                [username : 'lrollus', firstname : 'Loïc', lastname : 'Rollus', email : 'lrollus@ulg.ac.be', group : [[name : "GIGA"]], password : 'lR$2011', color : "#00FF00", roles : ["ROLE_USER", "ROLE_ADMIN"]],
+                [username : 'stevben', firstname : 'Benjamin', lastname : 'Stévens', email : 'bstevens@ulg.ac.be', group : [[name : "GIGA"]], password : 'sB$2011', color : "#0000FF",roles : ["ROLE_USER", "ROLE_ADMIN"]],
                 [username : 'pansen', firstname : 'Pierre', lastname : 'Ansen', email : 'pierreansen@gmail.com', group : [[name : "GIGA"]], password : 'pA$2013', color : "#FF0000", roles : ["ROLE_USER", "ROLE_ADMIN"]]
         ]
+
 
         SecRole.findByAuthority("ROLE_USER") ?: new SecRole(authority: "ROLE_USER").save(flush: true)
         SecRole.findByAuthority("ROLE_ADMIN") ?: new SecRole(authority: "ROLE_ADMIN").save(flush: true)
