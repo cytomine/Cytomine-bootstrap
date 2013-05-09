107da70505762e4c8bea1f6638e2a56430008e4b
stevben <b.stevens@ulg.ac.be>
2013-05-09 14:18:17 +0000

    Correction SearchTest
    Former-commit-id: 6d97946fe4fb4ea09ff9b0e3233605b90a53ca92

--- src/groovy/be/cytomine/test/http/SearchAPI.groovy
+++ src/groovy/be/cytomine/test/http/SearchAPI.groovy
@@ -1,14 +1,11 @@
 package be.cytomine.test.http
 
 import be.cytomine.test.Infos
-import be.cytomine.utils.SearchFilter
-import be.cytomine.utils.SearchOperator
-
 
 class SearchAPI extends DomainAPI {
 
     //LIST - Project ; Annotation ; Image
-    static def listDomain(String keywords, SearchOperator operator, SearchFilter filter, String username, String password) {
+    static def listDomain(String keywords, String operator, String filter, String username, String password) {
         String URL = Infos.CYTOMINEURL + "api/search?keywords=$keywords&operator=$operator&filter=$filter"
         return doGET(URL, username, password)
     }
