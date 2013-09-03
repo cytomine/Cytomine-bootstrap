2e866fa39830362f090839642b837145153ae1a6
loic911 <lrollus@ulg.ac.be>
2013-09-03 07:58:05 +0000

    Add missing files
    Former-commit-id: 31276941b4064a2073a6f4b62710542a6998a864

---
+++ grails-app/domain/be/cytomine/utils/News.groovy
@@ -0,0 +1,41 @@
+package be.cytomine.utils
+
+import be.cytomine.CytomineDomain
+import be.cytomine.security.User
+import grails.converters.JSON
+import org.apache.log4j.Logger
+
+/**
+ * Cytomine new
+ * Ex: "2013/08/10: Data from job can now be deleted"
+ */
+class News extends CytomineDomain {
+
+    Date added
+    String text
+    User user
+
+    static constraints = {
+        text(type: 'text',nullable: false)
+    }
+
+    static mapping = {
+        id generator: "assigned"
+        text type: 'text'
+        sort "id"
+    }
+
+    static void registerMarshaller() {
+         Logger.getLogger(this).info("Register custom JSON renderer for " + News.class)
+         JSON.registerObjectMarshaller(News) { news ->
+             def returnArray = [:]
+             returnArray['class'] = news.class
+             returnArray['text'] = news.text
+             returnArray['user'] = news.user.id
+             returnArray['created'] = news.added?.time?.toString()
+             returnArray['created'] = news.created?.time?.toString()
+             returnArray['updated'] = news.updated?.time?.toString()
+             return returnArray
+         }
+     }
+}
---
+++ grails-app/services/be/cytomine/utils/NewsService.groovy
@@ -0,0 +1,26 @@
+package be.cytomine.utils
+
+import be.cytomine.api.UrlApi
+import be.cytomine.image.ImageInstance
+import be.cytomine.ontology.AlgoAnnotation
+import be.cytomine.ontology.ReviewedAnnotation
+import be.cytomine.ontology.UserAnnotation
+import be.cytomine.project.Project
+import be.cytomine.security.SecUser
+import grails.util.Holders
+import groovy.sql.Sql
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pierre
+ * Date: 15/04/13
+ * Time: 15:38
+ * To change this template use File | Settings | File Templates.
+ */
+
+class NewsService extends ModelService {
+
+    def list() {
+        return News.list()
+    }
+}
