c44f06305441a5b1a0b78ee3f2fb304d2ac1fb7a
Benjamin Stévens <b.stevens@ulg.ac.be>
2014-06-03 21:27:18 +0000

    add missing files

---
+++ nginx/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sed "s/CORE_URL/$CORE_URL/g" /tmp/nginx.conf.sample  > /tmp/out.tmp1
+sed "s/IMS_URL/$IMS_URL/g" /tmp/out.tmp1 > /etc/nginx/nginx.conf
+rm /tmp/out.tmp1
+
+nginx
+
+tail -F /var/log/nginx/access.log
