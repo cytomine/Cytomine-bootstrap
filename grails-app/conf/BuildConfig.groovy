2fcfde0c624455b4b7757bb4a692cc35ab8351c4
Loïc Rollus <loicrollus@gmail.com>
2014-11-07 14:14:24 +0100

    fix CYTO-11, CYTO-12

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,7 +9,7 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.war.file = "target/${appName}.war"
 //grails.project.dependency.resolver="ivy"
 grails.project.dependency.resolver = "maven"
-grails.project.fork = [ test: false, run: false, war: false, console: false ]
+//grails.project.fork = [ test: false, run: false, war: false, console: false ]
 /*grails.project.fork = [
         // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
         //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
@@ -23,19 +23,19 @@ grails.project.fork = [ test: false, run: false, war: false, console: false ]
         // configure settings for the Console UI JVM
         console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ] */
-//grails.project.fork = [
-//        // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-//        //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-//        test: false,
-//        // configure settings for the test-app JVM, uses the daemon by default
-////        test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-//        // configure settings for the run-app JVM
-//        run: [maxMemory: 1024*6, minMemory: 1024*2, debug: false, maxPerm: 512, forkReserve:false],
-//        // configure settings for the run-war JVM
-//        war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-//        // configure settings for the Console UI JVM
-//        console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
-//]
+grails.project.fork = [
+        // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+        //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+        test: false,
+        // configure settings for the test-app JVM, uses the daemon by default
+//        test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+        // configure settings for the run-app JVM
+        run: [maxMemory: 1024*6, minMemory: 1024*2, debug: false, maxPerm: 512, forkReserve:false],
+        // configure settings for the run-war JVM
+        war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        // configure settings for the Console UI JVM
+        console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
 
 //grails.plugin.location."grails-jsondoc" = "/Users/stevben/Cytomine/github/grails-jsondoc"
 
