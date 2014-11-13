6f72a0ab715a9369d6cead50c76d33bde8a595a7
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2014-11-13 17:41:39 +0100

    JIRA HISTO-6

---
+++ grails-app/services/be/cytomine/project/ProjectDefaultLayerService.groovy
@@ -0,0 +1,91 @@
+package be.cytomine.project
+
+import be.cytomine.Exception.CytomineException
+import be.cytomine.command.AddCommand
+import be.cytomine.command.Command
+import be.cytomine.command.DeleteCommand
+import be.cytomine.command.EditCommand
+import be.cytomine.command.Transaction
+import be.cytomine.security.User
+import be.cytomine.utils.ModelService
+import be.cytomine.utils.Task
+import grails.transaction.Transactional
+
+import static org.springframework.security.acls.domain.BasePermission.READ
+import static org.springframework.security.acls.domain.BasePermission.WRITE
+
+@Transactional
+class ProjectDefaultLayerService extends ModelService {
+
+    static transactional = true
+    boolean saveOnUndoRedoStack = true
+
+    def springSecurityService
+    def transactionService
+    def securityACLService
+    def projectService
+    def secUserService
+
+    def currentDomain() {
+        return ProjectDefaultLayer
+    }
+
+    ProjectDefaultLayer read(def id) {
+        def layer = ProjectDefaultLayer.read(id)
+        if (layer) {
+            securityACLService.check(layer,READ)
+        }
+        layer
+    }
+
+    /**
+     * Get all default layers of the current project
+     * @return ProjectDefaultLayer list
+     */
+    def listByProject(Project project) {
+        securityACLService.check(project,READ)
+        return ProjectDefaultLayer.findAllByProject(project)
+    }
+
+    /**
+     * Add the new domain with JSON data
+     * @param json New domain data
+     * @return Response structure (created domain data,..)
+     */
+    def add(def json) throws CytomineException {
+        securityACLService.check(json.project,Project,WRITE)
+        User user = User.get(json.user)
+        return executeCommand(new AddCommand(user: user), null,json)
+    }
+
+    /**
+     * Update this domain with new data from json
+     * @param domain Domain to update
+     * @param jsonNewData New domain datas
+     * @return  Response structure (new domain data, old domain data..)
+     */
+    def update(ProjectDefaultLayer domain, def jsonNewData) throws CytomineException {
+        securityACLService.check(domain,WRITE)
+        User user = domain.getUser()
+        return executeCommand(new EditCommand(user: user),domain,jsonNewData)
+    }
+
+    /**
+     * Delete this domain
+     * @param domain Domain to delete
+     * @param transaction Transaction link with this command
+     * @param task Task for this command
+     * @param printMessage Flag if client will print or not confirm message
+     * @return Response structure (code, old domain,..)
+     */
+    def delete(ProjectDefaultLayer domain, Transaction transaction = null, Task task = null, boolean printMessage = true) {
+        securityACLService.check(domain.getProject(),WRITE)
+        User user = domain.getUser()
+        Command c = new DeleteCommand(user: user,transaction:transaction)
+        return executeCommand(c,domain,null, task)
+    }
+
+    def getStringParamsI18n(def domain) {
+        return [domain.id, domain.user.firstname+" "+domain.user.lastname]
+    }
+}
---
+++ test/functional/be/cytomine/ProjectDefaultLayerTests.groovy
@@ -0,0 +1,145 @@
+package be.cytomine
+
+import be.cytomine.project.Project
+import be.cytomine.project.ProjectDefaultLayer
+import be.cytomine.test.BasicInstanceBuilder
+import be.cytomine.test.Infos
+import be.cytomine.test.http.ProjectDefaultLayerAPI
+import grails.converters.JSON
+import org.codehaus.groovy.grails.web.json.JSONArray
+import org.codehaus.groovy.grails.web.json.JSONObject
+
+/**
+ * Created by hoyoux on 13.11.14.
+ */
+class ProjectDefaultLayerTests {
+
+    void testListAllProjectDefaultLayerByProjectWithCredential() {
+        ProjectDefaultLayer layer = BasicInstanceBuilder.getProjectDefaultLayer();
+        def result = ProjectDefaultLayerAPI.list(layer.project.id, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code
+        def json = JSON.parse(result.data)
+        assert json.collection instanceof JSONArray
+    }
+
+    void testListAllProjectDefaultLayerByProjectWithoutCredential() {
+        Project project = BasicInstanceBuilder.getProjectDefaultLayer().getProject();
+        def result = ProjectDefaultLayerAPI.list(project.id, Infos.BADLOGIN, Infos.BADPASSWORD)
+        assert 401 == result.code
+    }
+
+    void testShowProjectDefaultLayerWithCredential() {
+        ProjectDefaultLayer layer = BasicInstanceBuilder.getProjectDefaultLayer()
+        def result = ProjectDefaultLayerAPI.show(layer.id, layer.project.id, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code
+        def json = JSON.parse(result.data)
+        assert json instanceof JSONObject
+    }
+
+    void testAddProjectDefaultLayerCorrect() {
+        def layerToAdd = BasicInstanceBuilder.getProjectDefaultLayerNotExist()
+
+        def result = ProjectDefaultLayerAPI.create(layerToAdd.encodeAsJSON(), Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code
+        int idLayer = result.data.id
+
+        result = ProjectDefaultLayerAPI.show(idLayer, layerToAdd.project.id, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code
+
+        result = ProjectDefaultLayerAPI.undo()
+        assert 200 == result.code
+
+        result = ProjectDefaultLayerAPI.show(idLayer, layerToAdd.project.id, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 404 == result.code
+
+        result = ProjectDefaultLayerAPI.redo()
+        assert 200 == result.code
+
+        result = ProjectDefaultLayerAPI.show(idLayer, layerToAdd.project.id, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code
+    }
+
+    void testAddProjectDefaultLayerAlreadyExist() {
+        def layerToAdd = BasicInstanceBuilder.getProjectDefaultLayerNotExist(false, true);
+
+        def result = ProjectDefaultLayerAPI.create(layerToAdd.encodeAsJSON(), Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code // first creation
+
+        layerToAdd.hideByDefault = !layerToAdd.hideByDefault
+
+        result = ProjectDefaultLayerAPI.create(layerToAdd.encodeAsJSON(), Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 409 == result.code // cannot create if already exist
+    }
+
+    void testDeleteProjectDefaultLayer() {
+        def layerToDelete = BasicInstanceBuilder.getProjectDefaultLayerNotExist()
+        assert layerToDelete.save(flush: true)!= null
+        def id = layerToDelete.id
+        def idProject = layerToDelete.project.id
+        def result = ProjectDefaultLayerAPI.delete(id, idProject, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code
+
+        def showResult = ProjectDefaultLayerAPI.show(id, idProject, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 404 == showResult.code
+
+        result = ProjectDefaultLayerAPI.undo()
+        assert 200 == result.code
+
+        result = ProjectDefaultLayerAPI.show(id, idProject, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code
+
+        result = ProjectDefaultLayerAPI.redo()
+        assert 200 == result.code
+
+        result = ProjectDefaultLayerAPI.show(id, idProject, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 404 == result.code
+    }
+
+    void testUpdateProjectDefaultLayer() {
+        def layerToUpdate = BasicInstanceBuilder.getProjectDefaultLayerNotExist()
+        assert layerToUpdate.save(flush: true)!= null
+        def id = layerToUpdate.id
+        def idProject = layerToUpdate.project.id
+
+        def json
+
+        def showResult = ProjectDefaultLayerAPI.show(id, idProject, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == showResult.code
+
+        json = JSON.parse(showResult.data)
+
+        assert json.hideByDefault == false
+
+        layerToUpdate.hideByDefault = ! layerToUpdate.hideByDefault
+
+        def result = ProjectDefaultLayerAPI.update(id, layerToUpdate.encodeAsJSON(), Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code
+
+        showResult = ProjectDefaultLayerAPI.show(id, idProject, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == showResult.code
+        json = JSON.parse(showResult.data)
+        assert json.hideByDefault == true
+
+        result = ProjectDefaultLayerAPI.undo()
+        assert 200 == result.code
+
+        showResult = ProjectDefaultLayerAPI.show(id, idProject, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code
+        json = JSON.parse(showResult.data)
+        assert json.hideByDefault == false
+
+        result = ProjectDefaultLayerAPI.redo()
+        assert 200 == result.code
+
+        showResult = ProjectDefaultLayerAPI.show(id, idProject, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 200 == result.code
+        json = JSON.parse(showResult.data)
+        assert json.hideByDefault == true
+    }
+
+    void testDeleteProjectDefaultLayerNotExist() {
+        def result = ProjectDefaultLayerAPI.delete(-99, -99, Infos.SUPERADMINLOGIN, Infos.SUPERADMINPASSWORD)
+        assert 404 == result.code
+    }
+
+}
