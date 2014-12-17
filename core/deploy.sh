972028f5479ad00bf5f55df980e653f2e49ebbb7
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2014-12-17 09:34:27 +0100

    Use remote WAR

--- core/deploy.sh
+++ core/deploy.sh
@@ -2,48 +2,45 @@
 
 /etc/init.d/ssh start
 
-#echo Starting "$WAR_URL" 
+echo Starting "$WAR_URL" 
 #Copy the war file from mounted directory to tomcat webapps directory
-#if [ ! -z "$WAR_URL" ]
-#then
-rm -r /var/lib/tomcat7/webapps/*
-cd /var/lib/tomcat7/webapps/  #&& wget -q $WAR_URL -O ROOT.war
+if [ ! -z "$WAR_URL" ]
+then
+	rm -r /var/lib/tomcat7/webapps/*
+	cd /var/lib/tomcat7/webapps/  && wget -q $WAR_URL -O ROOT.war
 
-cp /tmp/ROOT.war /var/lib/tomcat7/webapps/ROOT.war
-chmod 777 /var/lib/tomcat7/webapps/ROOT.war
+	mkdir -p /usr/share/tomcat7/.grails
+	cd /usr/share/tomcat7/.grails
 
-mkdir -p /usr/share/tomcat7/.grails
-cd /usr/share/tomcat7/.grails
+	cp /tmp/cytomineconfig.groovy ./
 
-cp /tmp/cytomineconfig.groovy ./
+	### transform the ims urls for the config file ###
+	arr=$(echo $IMS_URLS | tr "," "\n")
+	arr=$(echo $arr | tr "[" "\n")
+	arr=$(echo $arr | tr "]" "\n")
 
-### transform the ims urls for the config file ###
-arr=$(echo $IMS_URLS | tr "," "\n")
-arr=$(echo $arr | tr "[" "\n")
-arr=$(echo $arr | tr "]" "\n")
+	IMS_URLS="["
+	for x in $arr
+	do
+	    IMS_URLS="${IMS_URLS}'http://$x',"
+	done
+	IMS_URLS="${IMS_URLS%?}"
+	IMS_URLS="$IMS_URLS]"
 
-IMS_URLS="["
-for x in $arr
-do
-    IMS_URLS="${IMS_URLS}'http://$x',"
-done
-IMS_URLS="${IMS_URLS%?}"
-IMS_URLS="$IMS_URLS]"
+	### END transform the ims urls for the config file ###
 
-### END transform the ims urls for the config file ###
+	echo "grails.serverURL='http://$CORE_URL'" >> cytomineconfig.groovy
+	echo "storage_buffer='$IMS_BUFFER_PATH'" >> cytomineconfig.groovy
+	echo "storage_path='$IMS_STORAGE_PATH'" >> cytomineconfig.groovy
 
-echo "grails.serverURL='http://$CORE_URL'" >> cytomineconfig.groovy
-echo "storage_buffer='$IMS_BUFFER_PATH'" >> cytomineconfig.groovy
-echo "storage_path='$IMS_STORAGE_PATH'" >> cytomineconfig.groovy
+	echo "grails.imageServerURL=$IMS_URLS" >> cytomineconfig.groovy
+	echo "grails.retrievalServerURL =['http://$RETRIEVAL_URL']" >> cytomineconfig.groovy
+	echo "grails.mongo.host = 'mongodb'" >> cytomineconfig.groovy
 
-echo "grails.imageServerURL=$IMS_URLS" >> cytomineconfig.groovy
-echo "grails.retrievalServerURL =['http://$RETRIEVAL_URL']" >> cytomineconfig.groovy
-echo "grails.mongo.host = 'mongodb'" >> cytomineconfig.groovy
-
-echo "grails.uploadURL='http://$UPLOAD_URL:81'" >> cytomineconfig.groovy
+	echo "grails.uploadURL='http://$UPLOAD_URL:81'" >> cytomineconfig.groovy
 
 
-#fi
+fi
 
 if [ $IS_LOCAL = true ]; then
 	echo "#Custom adding" >> /etc/hosts
--- ims/deploy.sh
+++ ims/deploy.sh
@@ -21,29 +21,26 @@ chown -R tomcat7:tomcat7 $IMS_STORAGE_PATH
 
 export LD_LIBRARY_PATH=/usr/local/lib/openslide-java
 
-#echo Starting "$WAR_URL" 
+echo Starting "$WAR_URL" 
 #Copy the war file from mounted directory to tomcat webapps directory
-#if [ ! -z "$WAR_URL" ]
-#then
-rm -r /var/lib/tomcat7/webapps/*
-cd /var/lib/tomcat7/webapps/ # && wget -q $WAR_URL -O ROOT.war
+if [ ! -z "$WAR_URL" ]
+then
+	rm -r /var/lib/tomcat7/webapps/*
+	cd /var/lib/tomcat7/webapps/ && wget -q $WAR_URL -O ROOT.war
 
-cp /tmp/ROOT.war ./ROOT.war
-chmod 777 /var/lib/tomcat7/webapps/ROOT.war
-
-mkdir -p /usr/share/tomcat7/.grails
-cd /usr/share/tomcat7/.grails
-touch imageserverconfig.properties
-echo "dataSource.url=jdbc:h2:/tmp/devDb;MVCC=TRUE;LOCK_TIMEOUT=10000" >> imageserverconfig.properties
-echo "cytomine.storageBufferPath=$IMS_BUFFER_PATH" >> imageserverconfig.properties
-echo "cytomine.imageServerPublicKey=4a5c7004-b6f8-4705-a118-c15d5c90dcdb" >> imageserverconfig.properties
-echo "cytomine.imageServerPrivateKey=70f35a45-c317-405a-8056-353db3d2bf56" >> imageserverconfig.properties
-echo "cytomine.vips=/usr/local/bin/vips" >> imageserverconfig.properties
-echo "cytomine.identify=identify" >> imageserverconfig.properties
-echo "cytomine.tiffinfo=tiffinfo" >> imageserverconfig.properties
-echo "cytomine.vipsthumbnail=/usr/local/bin/vipsthumbnail" >> imageserverconfig.properties
-echo "cytomine.iipImageServer=http://$IIP_URL:81/fcgi-bin/iipsrv.fcgi" >> imageserverconfig.properties
-#fi
+	mkdir -p /usr/share/tomcat7/.grails
+	cd /usr/share/tomcat7/.grails
+	touch imageserverconfig.properties
+	echo "dataSource.url=jdbc:h2:/tmp/devDb;MVCC=TRUE;LOCK_TIMEOUT=10000" >> imageserverconfig.properties
+	echo "cytomine.storageBufferPath=$IMS_BUFFER_PATH" >> imageserverconfig.properties
+	echo "cytomine.imageServerPublicKey=4a5c7004-b6f8-4705-a118-c15d5c90dcdb" >> imageserverconfig.properties
+	echo "cytomine.imageServerPrivateKey=70f35a45-c317-405a-8056-353db3d2bf56" >> imageserverconfig.properties
+	echo "cytomine.vips=/usr/local/bin/vips" >> imageserverconfig.properties
+	echo "cytomine.identify=identify" >> imageserverconfig.properties
+	echo "cytomine.tiffinfo=tiffinfo" >> imageserverconfig.properties
+	echo "cytomine.vipsthumbnail=/usr/local/bin/vipsthumbnail" >> imageserverconfig.properties
+	echo "cytomine.iipImageServer=http://$IIP_URL:81/fcgi-bin/iipsrv.fcgi" >> imageserverconfig.properties
+fi
 service tomcat7 start
 
 echo "/var/log/tomcat7/catalina.out {"   > /etc/logrotate.d/tomcat7
