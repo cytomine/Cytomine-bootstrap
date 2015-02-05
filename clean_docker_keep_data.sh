9058684526e5742c7ce0a659879f5acc4ce3c2c9
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2015-02-05 14:41:17 +0100

    fix docker space problem

--- clean_docker_keep_data.sh
+++ clean_docker_keep_data.sh
@@ -1,38 +1,38 @@
 docker stop memcached1
-docker rm memcached1
+docker rm -v memcached1
 docker stop memcached2
-docker rm memcached2
+docker rm -v memcached2
 docker stop memcached3
-docker rm memcached3
+docker rm -v memcached3
 docker stop memcached4
-docker rm memcached4
+docker rm -v memcached4
 docker stop rabbitmq
-docker rm rabbitmq
+docker rm -v rabbitmq
 docker stop mongodb
-docker rm mongodb
+docker rm -v mongodb
 docker stop retrievaldb
-docker rm retrievaldb
+docker rm -v retrievaldb
 docker stop db
-docker rm db
+docker rm -v db
 docker stop backup_postgis
-docker rm backup_postgis
+docker rm -v backup_postgis
 docker stop backup_retrieval
-docker rm backup_retrieval
+docker rm -v backup_retrieval
 docker stop backup_mongo
-docker rm backup_mongo
+docker rm -v backup_mongo
 docker stop iipOff
-docker rm iipOff
+docker rm -v iipOff
 docker stop iipCyto
-docker rm iipCyto
+docker rm -v iipCyto
 docker stop iipVent
-docker rm iipVent
+docker rm -v iipVent
 docker stop iipJ2
-docker rm iipJ2
+docker rm -v iipJ2
 docker stop ims
-docker rm ims
+docker rm -v ims
 docker stop core
-docker rm core
+docker rm -v core
 docker stop retrieval
-docker rm retrieval
+docker rm -v retrieval
 docker stop nginx
-docker rm nginx
+docker rm -v nginx
