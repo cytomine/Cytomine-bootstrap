a98578586bb2ae184bc70d5ff6b7dcd669423959
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2015-03-05 10:26:28 +0100

    improve script for war upgrade

--- update_core_war.sh
+++ update_core_war.sh
@@ -1,7 +1,7 @@
 docker stop core
-docker rm core
+docker rm -v core
 docker stop nginx
-docker rm nginx
+docker rm -v nginx
 
 sh create_docker_images.sh
 sh start_deploy.sh
--- update_ims_war.sh
+++ update_ims_war.sh
@@ -1,7 +1,7 @@
 docker stop ims
-docker rm ims
+docker rm -v ims
 docker stop nginx
-docker rm nginx
+docker rm -v nginx
 
 sh create_docker_images.sh
 sh start_deploy.sh
