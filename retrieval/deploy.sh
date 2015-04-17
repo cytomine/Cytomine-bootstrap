a0f914ea433093f180733ad636493caae5d007a6
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2015-04-17 10:36:08 +0200

    add license files

--- retrieval/deploy.sh
+++ retrieval/deploy.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#
+# Copyright (c) 2009-2015. Authors: see NOTICE file.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 #/etc/init.d/ssh start
 
@@ -30,6 +45,8 @@ else
 	java -jar retrieval-0.2-SNAPSHOT.war --spring.profiles.active=prod --retrieval.store.name=REDIS --retrieval.thumb.index=$RETRIEVAL_FOLDER/index --retrieval.thumb.search=$RETRIEVAL_FOLDER/search
 fi
 
+touch password.txt
+#echo "password must be put here" > password.txt
 
 touch test.out
 tail -F /tmp/test.out
