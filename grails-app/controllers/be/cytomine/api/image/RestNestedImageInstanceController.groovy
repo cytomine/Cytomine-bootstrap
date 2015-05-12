72074eb2bee571dc27096c4622982c0db1d9bde1
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2015-05-12 15:00:05 +0200

    add license files : part 1

--- grails-app/controllers/be/cytomine/api/image/RestNestedImageInstanceController.groovy
+++ grails-app/controllers/be/cytomine/api/image/RestNestedImageInstanceController.groovy
@@ -1,5 +1,21 @@
 package be.cytomine.api.image
 
+/*
+* Copyright (c) 2009-2015. Authors: see NOTICE file.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 import be.cytomine.Exception.CytomineException
 import be.cytomine.api.RestController
 import be.cytomine.image.ImageInstance
@@ -19,7 +35,6 @@ import org.restapidoc.pojo.RestApiParamType
 @RestApi(name = "nested image services", description = "Methods for managing a nested image, a sub-image of an existing image instance")
 class RestNestedImageInstanceController extends RestController {
 
-    def segmentationService
     def imageProcessingService
     def nestedImageInstanceService
     def imageInstanceService
