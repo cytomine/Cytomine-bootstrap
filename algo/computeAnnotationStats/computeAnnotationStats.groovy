92e5cdba9bd70cb612df5ad8f8471038427a9d74
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2015-05-12 15:04:49 +0200

    add license files : part 2

--- algo/computeAnnotationStats/computeAnnotationStats.groovy
+++ algo/computeAnnotationStats/computeAnnotationStats.groovy
@@ -1,3 +1,19 @@
+/*
+* Copyright (c) 2009-2015. Authors: see NOTICE file.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 //host:
 //publicKey:
 //privateKey:
@@ -11,13 +27,10 @@ groovy -cp "algo/computeAnnotationStats/Cytomine-Java-Client.jar:algo/computeAnn
 
  */
 
-import com.vividsolutions.jts.geom.*
+import be.cytomine.client.*
+import be.cytomine.client.collections.*
+import be.cytomine.client.models.*
 import com.vividsolutions.jts.io.WKTReader
-import be.cytomine.client.models.*;
-import be.cytomine.client.collections.*;
-import be.cytomine.client.*;
-import com.vividsolutions.jts.precision.SimpleGeometryPrecisionReducer;
-import com.vividsolutions.jts.geom.PrecisionModel;
 
 def sleep = 0
 
