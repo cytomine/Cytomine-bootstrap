36a709417b89a8e256b90992b647b75facc0af25
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2015-06-13 12:04:54 +0200

    iris deployment + clean

--- clean_docker_keep_data.sh
+++ clean_docker_keep_data.sh
@@ -24,14 +24,10 @@ docker stop memcached4
 docker rm -v memcached4
 docker stop mongodb
 docker rm -v mongodb
-docker stop retrievaldb
-docker rm -v retrievaldb
 docker stop db
 docker rm -v db
 docker stop backup_postgis
 docker rm -v backup_postgis
-docker stop backup_retrieval
-docker rm -v backup_retrieval
 docker stop backup_mongo
 docker rm -v backup_mongo
 docker stop iipOff
@@ -48,11 +44,13 @@ docker stop ims
 docker rm -v ims
 docker stop core
 docker rm -v core
-docker stop data_test
-docker rm -v data_test
 docker stop retrieval
 docker rm -v retrieval
+docker stop iris
+docker rm -v iris
 docker stop nginx
 docker rm -v nginx
 docker stop software_router
 docker rm -v software_router
+docker stop data_test
+docker rm -v data_test
