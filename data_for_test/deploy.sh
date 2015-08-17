0ef33d64047a95a8c9edd574b6aaa313ee6b7bc1
Renaud Hoyoux <renaud.hoyoux@yahoo.fr>
2015-08-17 18:21:52 +0200

    fix images creation

--- data_for_test/deploy.sh
+++ data_for_test/deploy.sh
@@ -21,11 +21,6 @@ if [ $IS_LOCAL = true ]; then
 	echo "$(route -n | awk '/UG[ \t]/{print $2}')       $UPLOAD_URL" >> /etc/hosts
 fi
 
-sed -i "s/PUBLIC_KEY/$PUBLIC_KEY/g" /tmp/script.groovy
-sed -i "s/PRIVATE_KEY/$PRIVATE_KEY/g" /tmp/script.groovy
-sed -i "s/UPLOAD_URL/$UPLOAD_URL/g" /tmp/script.groovy
-sed -i "s/CORE_URL/$CORE_URL/g" /tmp/script.groovy
-
 if [ ! -z "$JAVA_CLIENT_JAR" ]
 then
 	cd /tmp/  && wget -q $JAVA_CLIENT_JAR -O Cytomine-client-java.jar
