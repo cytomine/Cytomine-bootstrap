914d6656092f3878308c47f689da8ab62b4b5b38
TheGeekTortoise <Renaud.Hoyoux@yahoo.fr>
2020-03-02 15:51:42 +0100

    delete the front-end dist folder (and the binaries) from this repo and use the init.sh script to download the dist folder

--- init.sh
+++ init.sh
@@ -17,6 +17,14 @@
 #
 FILES=(configs/core/cytomineconfig.groovy configs/ims/imageserverconfig.properties configs/iipCyto/nginx.conf.sample configs/iipOff/nginx.conf.sample configs/nginx/nginx.conf configs/nginx/nginxDev.conf configs/nginx/dist/configuration.json configs/software_router/config.groovy start_deploy.sh hosts/core/addHosts.sh hosts/ims/addHosts.sh hosts/software_router/addHosts.sh hosts/slurm/addHosts.sh)
 
+if [ ! -d ./configs/nginx/dist ];
+then
+        echo "download compiled JS files"
+        wget https://github.com/cytomine/Cytomine-Web-UI/releases/download/v1.1.0/dist.zip -O dist.zip
+        unzip dist.zip -d configs/nginx
+        rm dist.zip
+fi
+
 #get all the config values.
 . ./configuration.sh
 
