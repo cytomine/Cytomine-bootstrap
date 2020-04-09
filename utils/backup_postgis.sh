98d15e2cda9be3c6ce246d96404fe3480a5f5434
TheGeekTortoise <Renaud.Hoyoux@yahoo.fr>
2020-04-09 19:03:21 +0200

    update years in license header

--- utils/backup_postgis.sh
+++ utils/backup_postgis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (c) 2009-2018. Authors: see NOTICE file.
+# Copyright (c) 2009-2020. Authors: see NOTICE file.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
